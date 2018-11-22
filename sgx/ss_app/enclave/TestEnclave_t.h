eb05fc227f26a03683b5355e4461d585b9cf4576
Kaushik Kasi <ksk@berkeley.edu>
2018-11-22 13:13:27 -0800

    all files

---
+++ sgx/ss_app/enclave/TestEnclave_t.h
@@ -0,0 +1,52 @@
+#ifndef TESTENCLAVE_T_H__
+#define TESTENCLAVE_T_H__
+
+#include <stdint.h>
+#include <wchar.h>
+#include <stddef.h>
+#include "sgx_edger8r.h" /* for sgx_ocall etc. */
+
+
+#include <stdlib.h> /* for size_t */
+
+#define SGX_CAST(type, item) ((type)(item))
+#  define BN_ULONG        unsigned long
+typedef struct bignum_st BIGNUM;
+
+ struct bignum_st
+        {
+        BN_ULONG *d;    /* Pointer to an array of 'BN_BITS2' bit chunks. */
+        int top;        /* Index of last used d +1. */
+        /* The next are internal book keeping for bn_expand. */
+        int dmax;       /* Size of the d array. */
+        int neg;        /* one if the number is negative */
+        int flags;
+        };
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+BIGNUM sign_single(unsigned char* message, int len);
+void generate_key_and_sign(void);
+void t_sgxssl_call_apis(void);
+void new_thread_func(void);
+void* get_ECDSA_sign_ADDR(void);
+void* get_BN_mod_aq_ADDR(void);
+void* get_BN_usub_ADDR(void);
+void* get_ECDSA_free_ADDR(void);
+
+sgx_status_t SGX_CDECL uprint(const char* str);
+sgx_status_t SGX_CDECL usgx_exit(int reason);
+sgx_status_t SGX_CDECL ucreate_thread(int* retval);
+sgx_status_t SGX_CDECL u_sgxssl_ftime(void* timeptr, uint32_t timeb_len);
+sgx_status_t SGX_CDECL sgx_oc_cpuidex(int cpuinfo[4], int leaf, int subleaf);
+sgx_status_t SGX_CDECL sgx_thread_wait_untrusted_event_ocall(int* retval, const void* self);
+sgx_status_t SGX_CDECL sgx_thread_set_untrusted_event_ocall(int* retval, const void* waiter);
+sgx_status_t SGX_CDECL sgx_thread_setwait_untrusted_events_ocall(int* retval, const void* waiter, const void* self);
+sgx_status_t SGX_CDECL sgx_thread_set_multiple_untrusted_events_ocall(int* retval, const void** waiters, size_t total);
+
+#ifdef __cplusplus
+}
+#endif /* __cplusplus */
+
+#endif
